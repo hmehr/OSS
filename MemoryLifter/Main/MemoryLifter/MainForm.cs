e0cb0203f859265a30a61ebb2ec0190a327eec1a
Hamid <hamid.smehr@gmail.com>
2013-11-19 22:22:31 -0500

    latest changes

--- MemoryLifter/Main/MemoryLifter/MainForm.cs
+++ MemoryLifter/Main/MemoryLifter/MainForm.cs
@@ -3,7 +3,7 @@
  * Contact: Learnlift USA, 12 Greenway Plaza, Suite 1510, Houston, Texas 77046, support@memorylifter.com					*
  *																								*
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License	*
- * as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.			*
+ * as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version			*
  *																								*
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty	*
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.	*
